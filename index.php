0bb66b5d1b803e05b43960ea8d6285c2ff94a77a
PDAD Soetta <pdad.soetta@gmail.com>
2019-10-18 10:49:53 +0700

    Add convert magic-command into real method name in index.php

--- index.php
+++ index.php
@@ -4,11 +4,23 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once 'DB/Database.php';
 require_once 'MySSOServer.php';
 
+// by bowie
+// here we map the so called 'magic-command' from Broker.php
+// into a real method name of the server class
+$commandMap = [
+    'change-password'   => 'changePassword'
+];
 
+// instantiate SSO Server
 $ssoServer = new MySSOServer();
 
+// grab command by checking $_REQUEST globals, defaulting to null
 $command = isset($_REQUEST['command']) ? $_REQUEST['command'] : null;
 
+// convert to real method name if possible
+$command = array_key_exists($command, $commandMap) ? $commandMap[$command] : $command;
+
+// continue as usual
 if (!$command || !method_exists($ssoServer, $command)) {
     header("HTTP/1.1 404 Not Found");
     header('Content-type: application/json; charset=UTF-8');
