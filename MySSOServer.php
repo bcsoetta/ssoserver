2085292bf7bb0916a587f4e386e0c8ab3b5800fb
PDAD Soetta <pdad.soetta@gmail.com>
2020-08-03 17:49:50 +0700

    set cache adapter to respect settings

--- MySSOServer.php
+++ MySSOServer.php
@@ -21,12 +21,12 @@ class MySSOServer extends Server {
     protected function createCacheAdapter()
     {
         $adapter = new Adapter\Memcached();
+        $adapter->setOption('ttl', $this->options['files_cache_ttl']);
         return new Cache($adapter);
     }
 
     protected function getBrokerInfo($brokerId) {
-        // $result = $this->db->select("SELECT apps.app_id, apps.secret FROM users_roles INNER JOIN apps ON users_apps.app_id = apps.app_id WHERE apps.app_id = '$brokerId'");
-        $result = $this->db->select("SELECT app_id, secret FROM apps WHERE app_id = '$brokerId'");
+        $result = $this->db->select("SELECT app_id, `secret` FROM aplikasi WHERE app_id = '$brokerId'");
         $brokers = mysqli_fetch_array($result, MYSQLI_ASSOC);
         if ($brokers > 0) {
             return $brokers;
@@ -43,7 +43,7 @@ class MySSOServer extends Server {
             return ValidationResult::error("password isn't set");
         }
 
-        $result = $this->db->select("SELECT * FROM users WHERE username = '$username' AND users.`status` = 'enabled'");
+        $result = $this->db->select("SELECT * FROM auth WHERE username = '$username' AND `status` = 'enabled'");
         $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
         if ($user['username'] !== $username) {
@@ -52,7 +52,6 @@ class MySSOServer extends Server {
 
         // generate pass
         // password_hash('123456', PASSWORD_DEFAULT);
-
         if (!password_verify($password, $user['password'])) {
             return ValidationResult::error('invalid password');
         }
@@ -61,49 +60,81 @@ class MySSOServer extends Server {
     }
 
     protected function getUserInfo($username) {
-        // $result = $this->db->select("SELECT users.user_id, users.username, users.name, users.nip, apps.app_id, apps.app_url, apps.app_name, apps.app_desc, apps.app_style FROM users_roles RIGHT JOIN users ON users_roles.user_id = users.user_id LEFT JOIN apps ON users_roles.app_id = apps.app_id WHERE users.username = '$username'");
-        // grab user data first
-        $qUserInfo = $this->db->select("SELECT a.user_id, a.username, a.name, a.nip, a.pangkat, b.kode, b.posisi, b.tempat, a.`status` FROM users a INNER JOIN ref_posisi b ON a.penempatan = b.kode WHERE a.username = '$username' AND a.`status` = 'enabled'");
-        $r = mysqli_fetch_assoc($qUserInfo);
 
-        // store in userInfo
-        $userInfo = $r;
-        // convert some data
-        $userInfo['status'] = $userInfo['status'] == "enabled";
-        $userInfo['apps_data'] = [];
+        $r = $this->db->select("SELECT `group` FROM auth WHERE username = '$username'");
+        list($group) = mysqli_fetch_array($r);
 
-        // grab user's roles in this
-        $result = $this->db->select("SELECT users_roles.role_id, apps.app_id, apps.app_name, apps.app_style, apps.app_desc, apps.app_url, roles.role_name, roles.role
-                                    FROM users_roles
-                                    RIGHT JOIN users ON users_roles.user_id = users.user_id
-                                    LEFT JOIN roles ON users_roles.role_id = roles.role_id
-                                    LEFT JOIN apps ON roles.app_id = apps.app_id
-                                    WHERE users.username = '$username' AND roles.`status` = 'enabled'");
+        $userInfo = [];
 
-        // append role data into userInfo
-        while ($row = mysqli_fetch_assoc($result)) {
-            $userInfo['apps_data'][$row['app_id']] = $userInfo['apps_data'][$row['app_id']] ?? [
-                'app_name'  => $row['app_name'],
-                'app_style' => $row['app_style'],
-                'app_desc' => $row['app_desc'],
-                'app_url' => $row['app_url'],
-                'roles'     => []
-                
-            ];
-            $userInfo['apps_data'][$row['app_id']]['roles'][] = $row['role_name'];
-            $userInfo['apps_data'][$row['app_id']]['rolex'][] = $row['role'];
+        // this is for customer
+        if ($group == 'customer') {
+            $qUser = $this->db->select("SELECT a.user_id, a.username, a.`group`, a.`status`, b.nama name, b.email, b.phone, b.nik, b.nib, b.tanggal_nib, b.npwp, b.perusahaan, b.alamat FROM auth a INNER JOIN customer b ON a.user_id = b.user_id WHERE a.username = '$username' AND a.`status` = 'enabled'");
+
+            $userInfo = mysqli_fetch_assoc($qUser);
+
+            $userInfo['status'] = $userInfo['status'] == 'enabled';
+            $userInfo['apps_data'] = [];
+
+            $qApp = $this->db->select("SELECT a.role_id, d.app_id, d.name app_name, d.style app_style, d.description app_desc, d.url app_url, d.sso app_sso, c.role_name, c.role FROM user_role a INNER JOIN auth x ON a.user_id = x.user_id RIGHT JOIN customer b ON a.user_id = b.user_id LEFT JOIN role c ON a.role_id = c.role_id LEFT JOIN aplikasi d ON c.app_id = d.app_id WHERE x.username = '$username' AND c.`status` = 'enabled'");
+
+            if ($qApp === false) {
+                $userInfo['apps_data'] = null;
+            } else {
+                if (mysqli_num_rows($qApp) > 0) {
+                    while ($d = mysqli_fetch_assoc($qApp)) {
+                        $userInfo['apps_data'][$d['app_id']] = $userInfo['apps_data'][$d['app_id']] ?? [
+                            'app_name' => $d['app_name'],
+                            'app_style' => $d['app_style'],
+                            'app_desc' => $d['app_desc'],
+                            'app_url' => $d['app_url'],
+                            'app_sso' => $d['app_sso'],
+                            'roles' => []
+                        ];
+                        $userInfo['apps_data'][$d['app_id']]['roles'][] = $d['role_name'];
+                        $userInfo['apps_data'][$d['app_id']]['rolex'][] = $d['role'];
+                    }
+                }
+            }
         }
+
+        // this is for beacukai
+        if ($group == 'beacukai') {
+            $qUser = $this->db->select("SELECT a.user_id, a.username, b.nama name, b.nip, b.pangkat, b.posisi kode, c.posisi, c.tempat, a.`group`, a.`status` FROM auth a INNER JOIN beacukai b ON a.user_id = b.user_id INNER JOIN ref_posisi c ON b.posisi = c.kode WHERE a.username = '$username' AND a.`status` = 'enabled'");
+
+            $userInfo = mysqli_fetch_assoc($qUser);
+
+            $userInfo['status'] = $userInfo['status'] == 'enabled';
+            $userInfo['apps_data'] = [];
+
+            $qApp = $this->db->select("SELECT a.role_id, d.app_id, d.name app_name, d.style app_style, d.description app_desc, d.url app_url, d.sso app_sso, c.role_name, c.role FROM user_role a INNER JOIN auth x ON a.user_id = x.user_id RIGHT JOIN beacukai b ON a.user_id = b.user_id LEFT JOIN role c ON a.role_id = c.role_id LEFT JOIN aplikasi d ON c.app_id = d.app_id WHERE x.username = '$username' AND c.status = 'enabled'");
+
+            if ($qApp === false) {
+                $userInfo['apps_data'] = null;
+            } else {
+                if (mysqli_num_rows($qApp) > 0) {
+                    while ($d = mysqli_fetch_assoc($qApp)) {
+                        $userInfo['apps_data'][$d['app_id']] = $userInfo['apps_data'][$d['app_id']] ?? [
+                            'app_name' => $d['app_name'],
+                            'app_style' => $d['app_style'],
+                            'app_desc' => $d['app_desc'],
+                            'app_url' => $d['app_url'],
+                            'app_sso' => $d['app_sso'],
+                            'roles' => []
+                        ];
+                        $userInfo['apps_data'][$d['app_id']]['roles'][] = $d['role_name'];
+                        $userInfo['apps_data'][$d['app_id']]['rolex'][] = $d['role'];
+                    }
+                }
+            }
+        }
+        
         return $userInfo;
     }
 
-    // ===============================================================================================================
-    // CUSTOM COMMAND
-    // ===============================================================================================================
-    // grab user data by role
+    // custom queries
     public function userByRole() {
         try {
             $data = $this->queryUserByRole($_REQUEST[0], $_REQUEST[1]);
-
             header('Content-type: application/json');
             echo json_encode([
                 'data' => $data
@@ -114,15 +145,12 @@ class MySSOServer extends Server {
         }
     }
 
-    // grab user by specific id
     public function userById() {
         try {
-            // strip parameter from request data
             $uid = $_REQUEST[0];
             $activeOnly = $_REQUEST[1] ?? false;
 
             $data = $this->queryUserById($uid, $activeOnly);
-
             header('Content-type: application/json');
             echo json_encode([
                 'data' => $data
@@ -133,116 +161,89 @@ class MySSOServer extends Server {
         }
     }
 
-    // ===============================================================================================================
-    // PROTECTED MEMBERS
-    // ===============================================================================================================
     protected function queryUserByRole($roles, $strict = false) {
-        // user must be sober to use this
         if (!is_array($roles)) {
-            throw new BadRequestException("Bad request bitch!", 400);
+            throw new BadRequestException("Bad reqest!", 400);
             return null;
         }
-
-        // flatten the array to quoted list of shiets
-        $role_flat = implode(",", array_map(function ($e) { return "'{$e}'"; }, $roles));
-
-        // build query string
+        $role_flat = implode(",", array_map(function($e) {return "'{$e}'"; }, $roles));
         $qString = "
         SELECT
             c.user_id,
-            c.username,
-            c.name,
+            b.username,
+            c.nama name,
             c.nip,
             c.pangkat,
-            c.penempatan,
-            c.status
+            c.posisi penempatan,
+            b.status
         FROM
-            users_roles a
+            user_role a
+            INNER JOIN auth b
+            ON
+                a.user_id = b.user_id
             JOIN
-            users c 
+            beacukai c
             ON
                 a.user_id = c.user_id
         WHERE
-            c.`status` <> 0
+            b.status <> 0
             AND
-            a.`status` <> 0
+            a.status <> 0
             AND
             a.role_id IN (
-                SELECT
-                    b.role_id
-                FROM
-                    roles b
-                WHERE
-                    b.role IN ({$role_flat})
+                SELECT d.role_id FROM role d WHERE d.role IN({$role_flat})
             )
         ";
-
         $result = $this->db->select($qString);
-
         if ($result === false) {
-            throw new NotFoundException("No data was found in the query", 404);
+            throw new NotFoundException("No data as found the the query", 404);
             return null;
         }
-
-        // must have succeed, go for it
-        // $data = $this->db->fetch()
         $data = [];
 
         while ($row = mysqli_fetch_assoc($result)) {
             $row['user_id'] = (int) $row['user_id'];
             $data[] = $row;
         }
-
         return $data;
     }
 
     protected function queryUserById($id, $activeOnly = false) {
         if (!isset($id)) {
-            throw new BadRequestException("Bad request bitch");
+            throw new BadRequestException("Bad reqest!", 400);
             return null;
         }
-        // escape parameter
         $uid = mysqli_escape_string($this->db->link, $id);
-
-        // return $uid;
-
-        // build query
         $qString = "
-        SELECT
-            c.user_id,
-            c.username,
-            c.name,
-            c.nip,
-            c.pangkat,
-            c.penempatan,
-            c.status
-        FROM
-            users c
-        WHERE
-            c.user_id = $uid
+            SELECT
+                a.user_id,
+                a.username,
+                c.nama name,
+                c.nip,
+                c.pangkat,
+                c.posisi penempatan,
+                a.status
+            FROM
+                beacukai c
+                INNER JOIN
+                    auth a ON c.user_id = a.user_id
+            WHERE a.user_id = $uid
         ";
-
         if ($activeOnly) {
-            $qString .= " AND c.status = 'enabled'";
+            $qString .= " AND a.status = 'enabled'";
         }
-
-        // return $qString;
-
-        // execute query
         $result = $this->db->select($qString);
-
         if ($result === false) {
             throw new NotFoundException("No user with id #{$id}", 404);
             return null;
         }
-
         $data = [];
 
         while ($row = mysqli_fetch_assoc($result)) {
             $row['user_id'] = (int) $row['user_id'];
-            $data = $row;
+            $data[] = $row;
         }
-
         return $data;
+
     }
 }
